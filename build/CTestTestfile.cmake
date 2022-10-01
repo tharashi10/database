b05090b7d56034c315479edd56492cafd9493782
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-10-01 15:09:47 +0900

    gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tharashi/2022/database
-# Build directory: /Users/tharashi/2022/database/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
